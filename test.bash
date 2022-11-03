dde02a04c4e1054d06be2faa6de69b9edcab244e
A1ka <s21c1033ar@s.chubakoudai.jp>
2022-11-03 12:26:02 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -14,14 +14,6 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng ${LINENO}
-[ " ${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]
-[ "${out}" = "" ] || ng ${LINENO}
-
 [ "$res" = 0 ] && echo OK
 exit $res
 
